091f0d3676d0cbd6d2c511d786030b0741909987
locckhl <locckhl123@gmail.com>
2021-08-12 16:30:29 +0700

    Add change bot nickname to btc price feature

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents } = require("discord.js");
+const binanceClient = require("./binanceConfig");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-require('dotenv').config()
+require("dotenv").config();
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  
+  setInterval(async () => {
+    // Get binace response
+    let res = await binanceClient.prices({ symbol: "BTCUSDT" });
+    let btcPrice = Math.round(res.BTCUSDT);
+
+    console.log(`BTCUSDT price is ${btcPrice}`);
+
+    // Change bot nickname to btc price in all server every 10 seconds
+    client.guilds.cache.map((guild) => {
+        guild.me.setNickname('BTC $'+ btcPrice);
+    });
+  
+  }, 10 * 1000);
 });
 
-client.on('interactionCreate', async interaction => {
+client.on("interactionCreate", async (interaction) => {
   if (!interaction.isCommand()) return;
 
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
+  if (interaction.commandName === "ping") {
+    await interaction.reply("Pong!");
   }
 });
 
